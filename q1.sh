dc2d3be9800717f9f14345f5815f63b863443456
idoniv01 <idobitton1@gmail.com>
2025-02-09 09:20:13 +0000

    adding an asnwer for question 1 - q1.sh

--- q1.sh
+++ q1.sh
@@ -1,1 +1,10 @@
-This is q1 answer
+#a
+awk 'END {print NR}' aliceinwonderland.txt
+#b
+awk '/ Alice / {count++} END {print count}' aliceinwonderland.txt
+#c
+awk '{for (i=1;i<=NF;i++) {words[]++}} END {for (k in words) if (words[k] == 1) print words[k],k}' aliceinwonderland.txt | wc -l
+#d
+awk '{for (i=1;i<=NF;i++) {words[]++}} END {for (k in words) print words[k], k}' aliceinwonderland.txt | sort -k1 -h -r | head -6 | sed '2d'
+#e
+awk '{for (i=1;i<=NF;i++) {len+=length(); count++}} END {print len/count}' aliceinwonderland.txt
