d656c1de4e0d4e29eaac4ae492f8fdc3f30091da
idoniv01 <idobitton1@gmail.com>
2025-02-10 18:59:14 +0000

    last edit to files q1.sh q2.sh - fixing bugs

--- q1.sh
+++ q1.sh
@@ -3,8 +3,8 @@ awk 'END {print NR}' aliceinwonderland.txt
 #b
 awk '/ Alice / {count++} END {print count}' aliceinwonderland.txt
 #c
-awk '{for (i=1;i<=NF;i++) {words[]++}} END {for (k in words) if (words[k] == 1) print words[k],k}' aliceinwonderland.txt | wc -l
+awk '{for (i=2;i<NF;i++) {words[$i]++}} END {for (k in words) if (words[k] == 1) print k}' aliceinwonderland.txt
 #d
-awk '{for (i=1;i<=NF;i++) {words[]++}} END {for (k in words) print words[k], k}' aliceinwonderland.txt | sort -k1 -h -r | head -6 | sed '2d'
+awk '{for (i=1;i<=NF;i++) {words[$i]++}} END {for (k in words) print words[k], k}' aliceinwonderland.txt | sort -k1 -h -r | head -6 | sed '2d'
 #e
-awk '{for (i=1;i<=NF;i++) {len+=length(); count++}} END {print len/count}' aliceinwonderland.txt
+awk '{for (i=1;i<=NF;i++) {len+=length($i); count++}} END {print len/count}' aliceinwonderland.txt
--- q2.sh
+++ q2.sh
@@ -1,10 +1,11 @@
 #a
-sed -E -n '/(Holmes|Sherlock)/p' sherlockholmes.txt | wc -l
+sed -rn '/(Holmes|Sherlock)/p' sherlockholmes.txt | wc -l
 #b
-sed -n 's/\(Sherlock\|Holmes\)/&\n/gp' sherlockholmes.txt | grep Sherlock|Holmes | wc -l
+sed -rn 's/\(Sherlock\|Holmes\)/p' sherlockholmes.txt | grep -oE "(Sherlock|Holmes)" | wc -l
 #c
-sed -n 's/^/Hello:/gp' sherlockholmes.txt
+sed -rn 's/^/Hello: /p' sherlockholmes.txt
 #d
 sed -E -n 's/[A-Z][a-z]{1,}\s[A-Z][a-z]{1,}/Ido Niv/gp' sherlockholmes.txt
 #e
-sed -E -n 's/\((.*)\)/[\1]/gp' sherlockholmes.txt
+sed -rn 's/\(/\[/gp' sherlockholmes.txt | sed -rn 's/\)/\]/gp'
+
